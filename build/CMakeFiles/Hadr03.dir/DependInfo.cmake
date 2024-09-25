e37260aa8bff290aeff2669c673a8fd979ad2e23
Guy Carter-Richardson <Guy.Carter-Richardson@outlook.com>
2024-09-25 12:01:27 +0100

    adding repository and testing build

---
+++ build/CMakeFiles/Hadr03.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/guy/software/geant4/geant4-v11.2.2-gdml-mt-install/projects/Hadr03/Hadr03.cc" "CMakeFiles/Hadr03.dir/Hadr03.cc.o" "gcc" "CMakeFiles/Hadr03.dir/Hadr03.cc.o.d"
+  "/home/guy/software/geant4/geant4-v11.2.2-gdml-mt-install/projects/Hadr03/src/ActionInitialization.cc" "CMakeFiles/Hadr03.dir/src/ActionInitialization.cc.o" "gcc" "CMakeFiles/Hadr03.dir/src/ActionInitialization.cc.o.d"
+  "/home/guy/software/geant4/geant4-v11.2.2-gdml-mt-install/projects/Hadr03/src/DetectorConstruction.cc" "CMakeFiles/Hadr03.dir/src/DetectorConstruction.cc.o" "gcc" "CMakeFiles/Hadr03.dir/src/DetectorConstruction.cc.o.d"
+  "/home/guy/software/geant4/geant4-v11.2.2-gdml-mt-install/projects/Hadr03/src/DetectorMessenger.cc" "CMakeFiles/Hadr03.dir/src/DetectorMessenger.cc.o" "gcc" "CMakeFiles/Hadr03.dir/src/DetectorMessenger.cc.o.d"
+  "/home/guy/software/geant4/geant4-v11.2.2-gdml-mt-install/projects/Hadr03/src/GammaNuclearPhysics.cc" "CMakeFiles/Hadr03.dir/src/GammaNuclearPhysics.cc.o" "gcc" "CMakeFiles/Hadr03.dir/src/GammaNuclearPhysics.cc.o.d"
+  "/home/guy/software/geant4/geant4-v11.2.2-gdml-mt-install/projects/Hadr03/src/GammaNuclearPhysicsLEND.cc" "CMakeFiles/Hadr03.dir/src/GammaNuclearPhysicsLEND.cc.o" "gcc" "CMakeFiles/Hadr03.dir/src/GammaNuclearPhysicsLEND.cc.o.d"
+  "/home/guy/software/geant4/geant4-v11.2.2-gdml-mt-install/projects/Hadr03/src/HistoManager.cc" "CMakeFiles/Hadr03.dir/src/HistoManager.cc.o" "gcc" "CMakeFiles/Hadr03.dir/src/HistoManager.cc.o.d"
+  "/home/guy/software/geant4/geant4-v11.2.2-gdml-mt-install/projects/Hadr03/src/PhysicsList.cc" "CMakeFiles/Hadr03.dir/src/PhysicsList.cc.o" "gcc" "CMakeFiles/Hadr03.dir/src/PhysicsList.cc.o.d"
+  "/home/guy/software/geant4/geant4-v11.2.2-gdml-mt-install/projects/Hadr03/src/PrimaryGeneratorAction.cc" "CMakeFiles/Hadr03.dir/src/PrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/Hadr03.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "/home/guy/software/geant4/geant4-v11.2.2-gdml-mt-install/projects/Hadr03/src/Run.cc" "CMakeFiles/Hadr03.dir/src/Run.cc.o" "gcc" "CMakeFiles/Hadr03.dir/src/Run.cc.o.d"
+  "/home/guy/software/geant4/geant4-v11.2.2-gdml-mt-install/projects/Hadr03/src/RunAction.cc" "CMakeFiles/Hadr03.dir/src/RunAction.cc.o" "gcc" "CMakeFiles/Hadr03.dir/src/RunAction.cc.o.d"
+  "/home/guy/software/geant4/geant4-v11.2.2-gdml-mt-install/projects/Hadr03/src/RunMessenger.cc" "CMakeFiles/Hadr03.dir/src/RunMessenger.cc.o" "gcc" "CMakeFiles/Hadr03.dir/src/RunMessenger.cc.o.d"
+  "/home/guy/software/geant4/geant4-v11.2.2-gdml-mt-install/projects/Hadr03/src/SteppingAction.cc" "CMakeFiles/Hadr03.dir/src/SteppingAction.cc.o" "gcc" "CMakeFiles/Hadr03.dir/src/SteppingAction.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
